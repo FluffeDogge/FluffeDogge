370e18aea8581f654716d1b24b9baf02ccbcc97c
FluffeDogge <63106529+FluffeDogge@users.noreply.github.com>
2020-04-03 14:07:11 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+let name = "Jack";
+let age = 14;
+let school = "1 Academy";
+
+console.log("Hi my name is " + name);
+name = "Imran";
+console.log("Hi, my name is " + name);
+
+console.log("I am" + age + " years old");
+console.log("I attend " + school);
+
+let numberOne;
+let numberTwo;
+let total;
+
+const addition = (numberOne, numberTwo) => {
+
+    total = numberOne + numberTwo;
+    console.log(total);
+};
+
+addition(20, 30);
+addition(30, 70);
+addition(40, 15);
